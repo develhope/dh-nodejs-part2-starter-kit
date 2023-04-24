2a4b49d93ae26d02780be4eeb53773b2e31d80a1
Simone Checco <simone.checco1995@gmail.com>
2023-04-24 16:57:01 +0200

    esercizio completo

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,30 @@
-const TODO: string = "start writing your Express API server here :)";
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+const app = express();
+const port = 3000;
 
-console.log(TODO);
+type Planet = {
+  id: number;
+  name: string;
+};
+
+type Planets = Planet[];
+
+let planets: Planets = [
+  { id: 1, name: "Earth" },
+  { id: 2, name: "Mars" },
+];
+app.use(morgan("dev"));
+app.get("/api/plantes", (req, res) => {
+  res.status(200).json(planets);
+});
+app.get("/api/plantes/:id", (req, res) => {
+  const { id } = req.params;
+  const planet = planets.find((p) => p.id === Number(id));
+  res.status(200).json(planet);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`);
+});
