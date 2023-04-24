166d9363e239108be4f5b4678c61e9eb413d35ab
Simone Checco <simone.checco1995@gmail.com>
2023-04-24 18:14:58 +0200

    esercizio completo

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,7 @@ let planets: Planets = [
   { id: 2, name: "Mars" },
 ];
 app.use(morgan("dev"));
+app.use(express.json());
 app.get("/api/plantes", (req, res) => {
   res.status(200).json(planets);
 });
@@ -24,7 +25,30 @@ app.get("/api/plantes/:id", (req, res) => {
   const planet = planets.find((p) => p.id === Number(id));
   res.status(200).json(planet);
 });
+app.post("/api/planets", (req, res) => {
+  const { id, name } = req.body;
+  const newPlanet = { id, name };
+  planets = [...planets, newPlanet];
 
+  res.status(201).json({ msg: "The planet was created" });
+});
+
+app.put("api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  planets = planets.map((p) => (p.id === Number(id) ? { ...p, name } : p));
+
+  console.log(planets);
+
+  res.status(200).json({ msg: "The planet was updated" });
+});
+
+app.delete("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  planets = planets.filter((p) => p.id !== Number(id));
+
+  res.status(200).json({ msg: "The planet was deleted" });
+});
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`);
 });
